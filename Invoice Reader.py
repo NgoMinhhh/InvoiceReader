dce37744a6a4ac09bb5dd066c73b2e7eedc0c34d
NgoMinhhh <ngonhatminh9898@gmail.com>
2022-02-07 00:19:56 +0700

    add template parsing and update var names

--- Invoice Reader.py
+++ Invoice Reader.py
@@ -5,11 +5,12 @@ import csv
 import datetime
 import logging
 import os
+from pathlib import Path
 from re import compile
-from tkinter import Tk
-from tkinter.filedialog import askdirectory
+from tkinter import Tk, filedialog
 
 import fitz  # This is pymupdf
+import yaml
 
 # For logging and debugging
 logging.basicConfig(level=logging.DEBUG,format=' %(asctime)s - %(levelname)s - %(message)s')
@@ -17,15 +18,14 @@ logging.debug('Start of program')
 
 # A class for invoice elements
 class inv_element:
-    inv_element_list = []
-    def __init__(self, name, pattern):
+    def __init__(self, name, pattern=None, flag=None):
         self.name   = name
         self.pattern  = pattern
-        self.__class__.inv_element_list.append(self)
+        self.flag = flag
 
     # Compile regex object and return the first non-null group in matching result
-    def getResult(self,text):
-        regex = compile(self.pattern)
+    def get_result(self,text):
+        regex = compile(self.pattern) if self.flag == None else compile(self.pattern,self.flag)
         group_count = self.pattern.count('|') + 1
         try:
             match = regex.search(text)
@@ -35,45 +35,54 @@ class inv_element:
         except AttributeError:
             return None 
 
+# Function to return a list of specific files from a specific folder path 
+def get_file_name_list(folder_path,file_type):
+    upper_file_type = file_type.upper()
+    file_name_list = [file for file in os.listdir(folder_path) if file.upper().endswith(upper_file_type)]
+    return file_name_list
+
 # Function to get all text from a pdf
-def getAllText(path):
+def get_inv_text(path):
     doc = fitz.open(path)
     text = ''
     for page in doc:
         text += page.get_text()
     return text
 
-### Create invoice elements 
-inv_number = inv_element('Invoice Number',r'Invoice No.*\n(.*)|Invoice Number\:(\d*)')
-inv_date = inv_element('Invoice Date',r'Invoice Date\:\n?(.*)')
-inv_curr = inv_element('Invoice Currency',r'Amount\((\w*)')
-inv_desc = inv_element('Invoice Description',r'Amount\(.*\)\n.\n(.*)|Amount\(.*\)\n(.*)')
-amt_beforeTax = inv_element('Amount before Tax',r'Amount\(.*\)\n.*\n(\d+.*)|(\d+.*)\nOutput Tax')
-amt_tax = inv_element('Amount of Tax',r'Output Tax\s?\n(.*)')
-amt_afterTax  = inv_element('Amount after Tax',r'Total Amount in\s?\(?.*\)?\n?(.*)')
-po_number = inv_element('PO Number', r'PO Number.*(.*)|PO No.*\n?(.*)')
-buyer_name = inv_element('Buyer Name',r'BILL TO PARTY.*\n(.*)|To\n(.*)')
-buyer_taxcode = inv_element(' Buyer Tax code', r'VAT No\s?\:(.*)')
-seller_name = inv_element('Seller Name', r'REGISTERED\s?\n?OFFICE\s?.?\n?(.*)')
-seller_taxcode = inv_element('Seller Tax code',r'VAT NO\s?\:(.*)')
+## Get a list of available template file and one for its path
+dirname_ = Path.cwd()
+template_name_list = get_file_name_list(Path.joinpath(dirname_,'Template'),'.yaml')
+template_path_list = [Path.joinpath(dirname_,'Template',template_name_list[i]) for i in range(len(template_name_list))]
 
-### Get input prompt to select folder directory
-Tk().withdraw() 
-folderPath = askdirectory() 
+## Parse yaml template into nested dict of invoice element object
+template_element_dict = {}
+for i, template in enumerate(template_name_list):
+    yml_data = yaml.safe_load(open(template_path_list[i],encoding='utf-8'))
+    template_element_dict[template] = [inv_element(**yml_data[ele]) for ele in yml_data.keys()]
+
+## Get input prompt to select folder directory
+while True:
+    Tk().withdraw() 
+    userSelected_folder_path = filedialog.askdirectory() 
+    break
 
 ## Get all the pdf files in selected folder
-name_list = [pdf for pdf in os.listdir(folderPath) if pdf.upper().endswith('.PDF')]
-inv_list = {}
+pdf_name_list = get_file_name_list(userSelected_folder_path,'.PDF')
+
+## Dictionary to for DictWriter
+inv_data_dict = {}
 
 ### Create nested dict with each sub dict is the pdf
-for i in name_list:
-    inv_list[i] = {}
-    inv_list[i]['Path'] = os.path.join(folderPath,i) #Get path
-    inv_list[i]['Text'] = getAllText(inv_list[i].get('Path'))
+for pdf in pdf_name_list:
+    inv_data_dict[pdf] = {}
+    inv_data_dict[pdf]['Path'] = os.path.join(userSelected_folder_path,pdf) #Get path
+    inv_data_dict[pdf]['Text'] = get_inv_text(inv_data_dict[pdf].get('Path'))
     
+    #TODO: a detect if statement to determine which template to match regex
+    #inv_data_dict[pdf]['Provider'] = 
     # A loop to iterate through list of text and list of regex
-    for element in inv_element.inv_element_list:  
-        inv_list[i][element.name] = element.getResult(inv_list[i].get('Text'))
+    # for element in inv_element.inv_element_list:  
+    #     inv_data_dict[pdf][element.name] = element.get_result(inv_data_dict[pdf].get('Text'))
 
 ### Write invoice data list into a csv
 ## Get header for csv
@@ -86,10 +95,10 @@ dt = datetime.datetime.now()
 csv_dt = dt.strftime('%Y%m%d %H.%M.%S')
 
 ## Write CSV and save it at user selected folder
-with open(f'{folderPath}/output {csv_dt}.csv','w',newline='',encoding="utf-16") as output_csv:
+with open(f'{userSelected_folder_path}/output {csv_dt}.csv','w',newline='',encoding="utf-16") as output_csv:
     w = csv.DictWriter(output_csv, headers_csv, delimiter = '\t')
     w.writeheader()
-    for key,val in sorted(inv_list.items()):
+    for key,val in sorted(inv_data_dict.items()):
         row = {'File Name':key}
         row.update(val)
         w.writerow(row)
