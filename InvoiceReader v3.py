997fccd0bfff82251052a45c85b7287436b7b72d
NgoMinhhh <ngonhatminh9898@gmail.com>
2022-04-27 21:56:29 +0700

    add print statment to indicate start and end

--- InvoiceReader v3.py
+++ InvoiceReader v3.py
@@ -7,47 +7,53 @@ import sys
 from datetime import datetime
 import csv
 
+
 def main():
-    """ USE INVOICE2DATA TO EXTRACT DATA FROM PDF INVOICE 
-   
-    """
+    """ USE INVOICE2DATA TO EXTRACT DATA FROM PDF INVOICE  """
+
     # Prompt User for folder selection
     Tk().withdraw()
     selected_folder = Path(filedialog.askdirectory())
     if not Path.is_dir(selected_folder):   # Exit if cancel selection
         sys.exit()
+    print('Script starts')
 
     templates = []
     # Load templates from external folder if set.
-    templates += read_templates(Path(r"C:\Users\PC41.VNPC41\Documents\Workspace\InvoiceReader\v3\Templates"))
+    templates += read_templates(
+        Path.joinpath(Path.cwd(), 'Template'))
 
     # Load pdfs and extract data
     output = []
     for f in selected_folder.glob('*.pdf'):
-        res = extract_data(f, templates=templates, input_module=pdfminer_wrapper)
+        res = extract_data(f, templates=templates,
+                           input_module=pdfminer_wrapper)
         if res:
-            output.append(res) 
+            output.append(res)
         # f.close()
-    
+
     # Write result to CSV file, saved at selected folder
     output_created_time = datetime.now().strftime('%Y%m%d %H%M%S')
-    output_file = Path.joinpath(selected_folder,f'Output {output_created_time}.csv')
-    
-    with open(output_file, 'w',newline='',encoding='utf-16') as f:
-        w = csv.writer(f,delimiter="\t")
+    output_file = Path.joinpath(
+        selected_folder, f'Output {output_created_time}.csv')
+
+    with open(output_file, 'w', newline='', encoding='utf-16') as f:
+        w = csv.writer(f, delimiter="\t")
         # Write header
         for line in output:
             first_row = []
             for k, v in line.items():
                 first_row.append(k)
         w.writerow(first_row)
-       
+
        # Write content
         for line in output:
             csv_items = []
             for k, v in line.items():
                 csv_items.append(v)
             w.writerow(csv_items)
+    print(
+        f'Script Finished. Please head into {selected_folder} for output file.')
 
 
 if __name__ == "__main__":
